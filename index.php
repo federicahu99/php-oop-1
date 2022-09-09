854eb03c15962986696f98e3da648d84eb2e6732
feddericahu99 <federica.hu339@gmail.com>
2022-09-09 13:40:51 +0200

    created class Movie

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+// è definita una classe ‘Movie’
+// all'interno della classe sono dichiarate delle variabili d'istanza
+// all'interno della classe è definito un costruttore
+// all'interno della classe è definito almeno un metodo
+// vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+
+class Movie 
+{
+    public $title;
+    public $year;
+    public $language;
+    public $type;
+}
+
+
+?>
+
