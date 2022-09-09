c2eaf55c3b8b741442703c00da40f73bf821edb8
feddericahu99 <federica.hu339@gmail.com>
2022-09-09 14:13:48 +0200

    commented

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <?php 
 
 // è definita una classe ‘Movie’
-// all'interno della classe sono dichiarate delle variabili d'istanza
-// all'interno della classe è definito un costruttore
-// all'interno della classe è definito almeno un metodo
-// vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
-
 class Movie 
 {
+    // all'interno della classe sono dichiarate delle variabili d'istanza
     public $original_title;
     public $title;
     public $year;
     public $language;
     public $genre;
-
+    
+    // all'interno della classe è definito un costruttore
     public function __construct($original_title, $title, $year, $language, $type)
     {
         $this->original_title = $original_title;
@@ -22,15 +19,32 @@ class Movie
         $this->language = $language;
         $this->genre = $genre;
     }
-
+    
+    // all'interno della classe è definito almeno un metodo
     public function GetTitles() {
-        return $original_title . $title;
+        return $original_title .- $title;
     }
 }
 
+// istanze
 $movie1 = new Movie ('Fall', 'Fall', '2022', 'en', 'Romance');
 $movie2 = new Movie ('The Good Father', 'Il Padrino', '1990', 'en', 'Drammatic');
 $movie3 = new Movie ('Goodfellas', 'Quei Bravi Ragazzi', '2022', 'en', 'Suspance');
 
+// vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie</title>
+</head>
+<body>
+   
+</body>
+</html>
+
 
